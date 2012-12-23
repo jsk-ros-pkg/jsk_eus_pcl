e1bb0fcdeb5196f464fa837ebb3e324d605e8d22
Yohei Kakiuchi <youhei@jsk.t.u-tokyo.ac.jp>
2012-12-24 02:27:04 +0900

    fix: includeing hpp file for template

--- eus_pcl/include/eus_pcl/euspcl_recognition.h
+++ eus_pcl/include/eus_pcl/euspcl_recognition.h
@@ -4,11 +4,13 @@
 #include <pcl/features/feature.h>
 #include <pcl/features/normal_3d.h>
 #include <pcl/features/fpfh.h>
+#include <pcl/features/impl/fpfh.hpp>
 
 #include <pcl/recognition/implicit_shape_model.h>
+#include <pcl/recognition/impl/implicit_shape_model.hpp>
 
 // eus functions
 extern pointer PCL_ISM_TRAINING (register context *ctx, int n, pointer *argv);
-//extern pointer PCL_ISM_TRAINING (register context *ctx, int n, pointer *argv);
+extern pointer PCL_ISM_DETECTION (register context *ctx, int n, pointer *argv);
 
 #endif
